d8a8cdf0f044c11f4d26ac3aa395062a74c5ee41
Ilya <Ilya_11_11@mail.ru>
2020-11-04 21:52:05 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.css';
+
+export function sliderInitialization() {
+  let Flickity = require('flickity');
+  let flkt = new Flickity( '.slider', {
+    freeScroll: false,
+    wrapAround:  true,
+    pageDots: false,
+    lazyLoad: true,
+    draggable: false
+  });
+  let flkty = new Flickity('.sliderPhoto', {
+    freeScroll: false,
+    wrapAround:  true,
+    pageDots: false,
+    lazyLoad: true,
+    draggable: false
+  });
+  let flk = new Flickity('.sliderCards', {
+    freeScroll: false,
+    wrapAround:  true,
+    pageDots: true,
+    draggable: false,
+    groupCells: 2,
+    contain: true,
+    prevNextButtons: true,
+    percentPosition: false,
+    adaptiveHeight: true
+  })
+}
+
+sliderInitialization()
